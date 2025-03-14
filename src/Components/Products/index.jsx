4551918c8690570b492e1061b1dafa33422b3952
Medine Hemidova <maddyhamidova2010@gmail.com>
2025-03-14 14:30:45 +0400

    pagination is done

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -7,62 +7,101 @@ const Products = () => {
   const url = import.meta.env.VITE_BACKEND_URL;
   const [products, SetProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [count, setCount] = useState(4);
+  const [pages, SetPages] = useState(0);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(0);
+  const [activePage, setActivePage] = useState(1);
   useEffect(() => {
     axios
       .get(url)
       .then(({ data }) => {
         SetProducts(data);
-        setIsLoading(false)
+        SetPages(Math.ceil(data.length / count));
+        setIsLoading(false);
       })
       .catch((err) => {
-        setIsLoading(false)
+        setIsLoading(false);
         if (err.status === 404) {
           toast.error("backend ilə bağlı problem!");
         }
       });
   }, []);
+
+  useEffect(() => {
+    setEndIndex(startIndex + count);
+  }, [startIndex]);
+
+  useEffect(() => {
+    setStartIndex((activePage - 1) * count);
+    setEndIndex(activePage * count);
+  }, [activePage]);
+
   if (isLoading) {
     return <Loading />;
   }
+  if (pages) {
+    console.log();
+  }
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-6 px-5">
+    <>
       {products.length > 0 ? (
-        products.map(
-          ({ id, title, category, price, description, images, slug }) => {
-            return (
-              <div
-                key={id}
-                className="border-1 border-gray-300 relative rounded-md"
-              >
-                <div
-                  className="object-cover w-full h-[200px] rounded-tl-md rounded-tr-md bg-center bg-no-repeat bg-cover"
-                  style={{ backgroundImage: `url(${images[0]})` }}
-                />
-                <div className="p-2">
-                  <h2 className="truncate font-bold">{title}</h2>
-                  <h2 className="absolute top-2 right-2 bg-red-300 px-2 py-0.5 font-sm rounded-md">
-                    {category.name}
-                  </h2>
-                  <p className="my-2 line-clamp-2">{description}</p>
-                  <p className="truncate font-bold text-red-600 text-xl">
-                    {price} AZN
-                  </p>
-                </div>
-                <Link
-                  to={"/products/" + slug}
-                  className="absolute inset-0"
-                ></Link>
-              </div>
-            );
-          }
-        )
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-6 px-5">
+          {products
+            .slice(startIndex, endIndex)
+            .map(
+              ({ id, title, category, price, description, images, slug }) => {
+                return (
+                  <div
+                    key={id}
+                    className="border-1 border-gray-300 relative rounded-md"
+                  >
+                    <div
+                      className="object-cover w-full h-[200px] rounded-tl-md rounded-tr-md bg-center bg-no-repeat bg-cover"
+                      style={{ backgroundImage: `url(${images[0]})` }}
+                    />
+                    <div className="p-2">
+                      <h2 className="truncate font-bold">{title}</h2>
+                      <h2 className="absolute top-2 right-2 bg-red-300 px-2 py-0.5 font-sm rounded-md">
+                        {category.name}
+                      </h2>
+                      <p className="my-2 line-clamp-2">{description}</p>
+                      <p className="truncate font-bold text-red-600 text-xl">
+                        {price} AZN
+                      </p>
+                    </div>
+                    <Link
+                      to={"/products/" + slug}
+                      className="absolute inset-0"
+                    ></Link>
+                  </div>
+                );
+              }
+            )}
+        </div>
       ) : (
         <h2 className="text-2xl font-bold text-red-600 bg-gray-100 p-4 rounded-lg shadow-md">
           İstədiyiniz məlumat tapılmadı
         </h2>
       )}
-    </div>
+      <div className="flex justify-center items-center space-x-2 mt-4">
+        {new Array(pages).fill("").map((_, index) => {
+          return (
+            <div
+              className={`w-10 h-10 flex items-center justify-center text-white rounded-full text-lg font-semibold cursor-pointer 
+        hover:bg-slate-500 transition-all duration-300 shadow-md 
+        ${activePage === index + 1 ? "bg-slate-900" : "bg-gray-600"}`}
+              onClick={() => {
+                setActivePage(index + 1);
+              }}
+              key={index}
+            >
+              {index + 1}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
