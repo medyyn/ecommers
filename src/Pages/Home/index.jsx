2dd6a507193269cb409d7706e978435ba21191b8
Medine Hemidova <maddyhamidova2010@gmail.com>
2025-03-14 15:33:39 +0400

    added category in home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../Components/Layout/Navbar'
 import Products from '../../Components/Products'
+import Categories from '../../Components/Categories'
 
 const Home = () => {
+  const [searchedText, setSearchedText] = useState("")
+  const [activeCategory, setActiveCategory] = useState(0)
   return (
     <>
-        <Navbar/>
-        <Products/>
+        <Navbar searchedText={searchedText}
+        setSearchedText={setSearchedText}
+        />
+        <div className="px-5 grid grid-cols-[200px_1fr]">
+        <Categories/>
+        <Products searchedText={searchedText}/>
+        </div>
     </>
   )
 }
