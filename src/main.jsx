05a11d27c52486e96e1396915ea645b5ed44bd2e
Medine Hemidova <maddyhamidova2010@gmail.com>
2025-03-14 12:56:48 +0400

    import hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './style.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Toaster position="top-center" />
+    <App />
+  </BrowserRouter>
+);
