95ff71a75995c2df8543679e12669c9eb52ee308
Medine Hemidova <maddyhamidova2010@gmail.com>
2025-03-14 12:57:51 +0400

    added not found

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,42 +1,60 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { Link } from "react-router";
-
+import NotFound from "../NotFound";
 const Products = () => {
   const url = import.meta.env.VITE_BACKEND_URL;
   const [products, SetProducts] = useState([]);
+
   useEffect(() => {
-    axios.get(url).then(({ data }) => {
-      SetProducts(data);
-    });
+    axios
+      .get(url)
+      .then(({ data }) => {
+        SetProducts(data);
+      })
+      .catch((err) => {
+        if (err.status === 404) {
+          toast.error("backend ile bagli problem!");
+        }
+      });
   }, []);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-6 px-5">
-      {products.map(({ id, title, category, price, description, images, slug }) => {
-        return (
-          <div
-            key={id}
-            className="border-1 border-gray-300 relative rounded-md"
-          >
-            <div
-              alt={title}
-              className="object-cover w-full h-[200px] rounded-tl-md rounded-tr-md bg-center bg-no-repeat bg-cover"
-              style={{ backgroundImage: `url(${images[0]})` }}
-            />
-            <div className="p-2">
-              <h2 className="truncate font-bold">{title}</h2>
-              <h2 className="absolute top-2 right-2 bg-red-300 px-2 py-0.5 font-sm rounded-md">
-                {category.name}
-              </h2>
-              <p className="my-2 line-clamp-2">{description}</p>
-              <p className="truncate font-bold text-red-600 text-xl">
-                {price} AZN
-              </p>
-            </div>
-            <Link to={"/products/" + slug} className="absolute inset-0"></Link>
-          </div>
-        );
-      })}
+      {products.length > 0 ? (
+        products.map(
+          ({ id, title, category, price, description, images, slug }) => {
+            return (
+              <div
+                key={id}
+                className="border-1 border-gray-300 relative rounded-md"
+              >
+                <div
+                  className="object-cover w-full h-[200px] rounded-tl-md rounded-tr-md bg-center bg-no-repeat bg-cover"
+                  style={{ backgroundImage: `url(${images[0]})` }}
+                />
+                <div className="p-2">
+                  <h2 className="truncate font-bold">{title}</h2>
+                  <h2 className="absolute top-2 right-2 bg-red-300 px-2 py-0.5 font-sm rounded-md">
+                    {category.name}
+                  </h2>
+                  <p className="my-2 line-clamp-2">{description}</p>
+                  <p className="truncate font-bold text-red-600 text-xl">
+                    {price} AZN
+                  </p>
+                </div>
+                <Link
+                  to={"/products/" + slug}
+                  className="absolute inset-0"
+                ></Link>
+              </div>
+            );
+          }
+        )
+      ) : (
+        <NotFound />
+      )}
     </div>
   );
 };
--- src/Pages/ProductDetail/index.jsx
+++ src/Pages/ProductDetail/index.jsx
@@ -3,6 +3,8 @@ import Navbar from "../../Components/Layout/Navbar";
 import { useParams } from "react-router";
 import axios from "axios";
 import Rating from "../../Components/Rating";
+import toast from "react-hot-toast";
+import NotFound from "../../Components/NotFound";
 
 const ProductDetail = () => {
   const { slug } = useParams();
@@ -10,40 +12,53 @@ const ProductDetail = () => {
   const url = import.meta.env.VITE_BACKEND_URL;
   const [currentImage, setCurrentImage] = useState("");
   useEffect(() => {
-    axios.get(url).then(({ data }) => {
-      const currentElement = data.find((e) => e.slug === slug);
-      SetProduct(currentElement);
-      setCurrentImage(currentElement.images[0]);
-    });
+    axios
+      .get(url)
+      .then(({ data }) => {
+        const currentElement = data.find((e) => e.slug === slug);
+        SetProduct(currentElement);
+        setCurrentImage(currentElement.images[0]);
+      })
+      .catch((err) => {
+        if (err.response?.status === 404) {
+          toast.error("Backend ilə bağlı problem!");
+        }
+      });
   }, []);
   return (
     <>
       <Navbar />
-      <div className="grid grid-cols-2 px-5 gap-16 mt-4">
-        <div className="border-2 border-gray-300 p-4 grid grid-cols-[3fr_1fr] gap-4">
-          <img src={currentImage} alt="" className="w-full h-auto" />
+      {product.slug ? (
+        <div className="grid grid-cols-2 px-5 gap-16 mt-4">
+          <div className="border-2 border-gray-300 p-4 grid grid-cols-[3fr_1fr] gap-4">
+            <img src={currentImage} alt="" className="w-full h-auto" />
 
-          <div className="flex flex-col gap-2">
-            {product.images?.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                className="w-full h-auto
+            <div className="flex flex-col gap-2">
+              {product.images?.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  className="w-full h-auto
               cursor-pointer"
-                onClick={() => {
-                  setCurrentImage(image);
-                }}
-              />
-            ))}
+                  onClick={() => {
+                    setCurrentImage(image);
+                  }}
+                />
+              ))}
+            </div>
+          </div>
+          <div>
+            <h2 className="text-4xl font-bold text-slate-700">
+              {product.title}
+            </h2>
+            <p className="text-gray-700 my-3 text-xl">{product.description}</p>
+            <p className="text-3xl font-bold text-red-500">${product.price}</p>
+            <Rating />
           </div>
         </div>
-        <div>
-          <h2 className="text-4xl font-bold text-slate-700">{product.title}</h2>
-          <p className="text-gray-700 my-3 text-xl">{product.description}</p>
-          <p className="text-3xl font-bold text-red-500">${product.price}</p>
-          <Rating />
-        </div>
-      </div>
+      ) : (
+        <NotFound/>
+      )}
     </>
   );
 };
