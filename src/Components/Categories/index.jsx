0e3f6047e3a7d479deacad87250c7228e3b9e7bd
Medine Hemidova <maddyhamidova2010@gmail.com>
2025-03-14 16:09:18 +0400

    active Category added

--- src/Components/Categories/index.jsx
+++ src/Components/Categories/index.jsx
@@ -1,23 +1,41 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const Categories = () => {
+const Categories = ({ activeCategory, setActiveCategory }) => {
   const url = import.meta.env.VITE_CATEGORIES_URL;
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
-    axios.get(url).then(({data}) => {
-        setCategories(data)
-    })
-  },[])
-  return <div className="mt-5">
-    {
-        categories.map(({id,name}) => {
-            return <div key={id}>
-                {name}
-            </div>
-        })
-    }
-  </div>;
+    axios.get(url).then(({ data }) => {
+      setCategories(data);
+    });
+  }, []);
+  return (
+    <div className="mt-5">
+      <div
+        className={` p-2 mb-2 rounded-sm text-white 
+        cursor-pointer ${
+          activeCategory === "All" ? "bg-slate-800" : "bg-slate-600"
+        }`}
+        onClick={() => setActiveCategory("All")}
+      >
+        All
+      </div>
+      {categories.map(({ id, name}) => {
+        return (
+          <div
+            key={id}
+            className={` p-2 mb-2 rounded-sm 
+            text-white cursor-pointer ${
+              activeCategory === name ? "bg-slate-800" : "bg-slate-600"
+            }`}
+            onClick={() => setActiveCategory(name)}
+          >
+            {name}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
