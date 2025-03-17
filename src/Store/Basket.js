f40469ecdd503ffb765a03082a29cc3ec35c37aa
Medine Hemidova <maddyhamidova2010@gmail.com>
2025-03-17 15:37:45 +0400

    create a limit for buying product

--- src/Store/Basket.js
+++ src/Store/Basket.js
@@ -12,15 +12,25 @@ const UseBasket = create((set) => ({
     changeProductCount: (id, o) => set(state => {
         const currentElement = state.basket.find((p) => p.id === id)
         if (o === "+") {
-            currentElement.count += 1
-        } else if (o === "-"){
-            if (currentElement.count > 1){
+            if (currentElement.count < 10){
+                currentElement.count += 1
+            }
+        } else if (o === "-") {
+            if (currentElement.count > 1) {
                 currentElement.count -= 1
             }
         }
         currentElement.totalPrice = currentElement.count * currentElement.price
+        localStorage.setItem("basket", JSON.stringify([...state.basket]))
         return {
             basket: [...state.basket],
+        }
+    }),
+    deleteProductFromBasket: (id) => set(state => {
+        const allProducts = state.basket.filter((p) => p.id !== id)
+        localStorage.setItem("basket", JSON.stringify(allProducts))
+        return {
+            basket: allProducts,
         }
     })
 }))
