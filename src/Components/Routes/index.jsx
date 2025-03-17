0b3892668c352ac398a6a59eacf138a71d25abd5
Medine Hemidova <maddyhamidova2010@gmail.com>
2025-03-17 12:54:32 +0400

    added basket in routes

--- src/Components/Routes/index.jsx
+++ src/Components/Routes/index.jsx
@@ -3,11 +3,13 @@ import Home from '../../Pages/Home'
 import ProductDetail from '../../Pages/ProductDetail'
 import { Route, Routes } from 'react-router'
 import NotFound from '../NotFound'
+import Basket from '../../Pages/Basket'
 
 const WebRoutes = () => {
     const routes = [
         {id:0, path: "/", element: <Home/>},
         {id:1, path: "/products/:slug", element: <ProductDetail/>},
+        {id:2, path: "/basket", element: <Basket/>}
     ]
   return (
     <Routes>
